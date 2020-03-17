d7c65950d380c16810726e4dff8e097db9785b93
nalbam <me@nalbam.com>
2020-03-17 22:15:40 +0900

    lepton_buf

--- run.py
+++ run.py
@@ -118,6 +118,8 @@ def run():
                         continue
                     last_nr = nr
 
+                    print(lepton_buf)
+
                     cv2.normalize(lepton_buf, lepton_buf, 0, 65535, cv2.NORM_MINMAX)
                     np.right_shift(lepton_buf, 8, lepton_buf)
                     a[: lepton_buf.shape[0], : lepton_buf.shape[1], :] = lepton_buf
