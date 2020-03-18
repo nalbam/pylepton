2be8e32cc3cd80bf0fa01b5f26564e1b8fdec83e
nalbam <me@nalbam.com>
2020-03-18 13:42:48 +0900

    normalize

--- run.py
+++ run.py
@@ -123,7 +123,7 @@ def run():
                 # print(len(lepton_buf), len(lepton_buf[0]))
                 # print(lepton_buf[0][0], lepton_buf[50][50], lepton_buf[100][100])
 
-                cv2.normalize(lepton_buf, lepton_buf, 0, 1024, cv2.NORM_MINMAX)
+                cv2.normalize(lepton_buf, lepton_buf, 0, 65535, cv2.NORM_MINMAX)
                 np.right_shift(lepton_buf, 8, lepton_buf)
 
                 # print(lepton_buf[0][0], lepton_buf[50][50], lepton_buf[100][100])
